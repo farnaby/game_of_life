2351d0e2811301e51bd73a9d7f1302b108744d10
Daniel an der Brügge <farnaby@web.de>
2013-02-06 20:59:18 +0100

    Completing first functional test.

--- functional_test.py
+++ functional_test.py
@@ -7,18 +7,60 @@ PORT = 8111
 
 # TODO: start & stop server automatically
 
-class TestGameOfLife(unittest.TestCase):
+class GOLPlayer(object):
 
-    def setUp(self):
+    def __init__(self):
         self.fox = webdriver.Firefox(timeout=3)
         self.fox.get("http://localhost:{0}".format(PORT))
 
-    def tearDown(self):
+    def stop(self):
         self.fox.close()
+
+    def get_board(self):
+        return self.fox.find_element_by_id("board")
+
+    def get_squares(self):
+        board = self.get_board()
+        return board.find_elements_by_class_name("square")
+
+    def get_square(self, num):
+        squares = self.get_squares()
+        return squares[num]
+
+    def is_square_populated(self, num):
+        chosen_square = self.get_square(num)
+        return "populated" in chosen_square.get_attribute('class')
+
+    def click_square(self, num):
+        chosen_square = self.get_square(num)
+        chosen_square.click()
+
+
+
+class TestGameOfLife(unittest.TestCase):
+
+    def setUp(self):
+        self.player = GOLPlayer()
+
+    def tearDown(self):
+        self.player.stop()
 
     def test_board(self):
         """ The page should show a board with 30x30 fields. The colour can be changed via mouse click. """
-        self.fail("TODO: finish")
+        self.player.get_board()
+        squares = self.player.get_squares()
+        self.assertEqual(len(squares), 30*30)
+
+        self.assertFalse(self.player.is_square_populated(55))
+        self.assertFalse(self.player.is_square_populated(121))
+
+        self.player.click_square(55)
+        self.assertTrue(self.player.is_square_populated(55))
+        self.assertFalse(self.player.is_square_populated(121))
+
+        self.player.click_square(55)
+        self.assertFalse(self.player.is_square_populated(55))
+        self.assertFalse(self.player.is_square_populated(121))
 
 
 if __name__ == '__main__':
