b05f306eaefef8f2f52b4a014d04aa138b778af8
Daniel an der Brügge <farnaby@web.de>
2013-02-07 19:32:45 +0100

    Make ft simpler and more pythonish.

--- functional_test.py
+++ functional_test.py
@@ -7,59 +7,48 @@ PORT = 8111
 
 # TODO: start & stop server automatically
 
-class GOLPlayer(object):
+class SeleniumTestPlayer(unittest.TestCase):
+    """ Base class for testing Conway's Game of Life webapp, hiding selenium details. """
 
-    def __init__(self):
-        self.fox = webdriver.Firefox(timeout=3)
-        self.fox.get("http://localhost:{0}".format(PORT))
+    def setUp(self):
+        self._fox = webdriver.Firefox(timeout=3)
+        self._fox.get("http://localhost:{0}".format(PORT))
 
-    def stop(self):
-        self.fox.close()
+    def tearDown(self):
+        self._fox.close()
 
     def get_board(self):
-        return self.fox.find_element_by_id("board")
+        return self._fox.find_element_by_id("board")
 
     def get_squares(self):
         board = self.get_board()
         return board.find_elements_by_class_name("square")
 
-    def get_square(self, num):
-        squares = self.get_squares()
-        return squares[num]
+    def assert_populated(self, square):
+        self.assertIn("populated", square.get_attribute('class'))
 
-    def is_square_populated(self, num):
-        chosen_square = self.get_square(num)
-        return "populated" in chosen_square.get_attribute('class')
-
-    def click_square(self, num):
-        chosen_square = self.get_square(num)
-        chosen_square.click()
+    def assert_not_populated(self, square):
+        self.assertNotIn("populated", square.get_attribute('class'))
 
 
 
-class TestGameOfLife(unittest.TestCase):
-
-    def setUp(self):
-        self.player = GOLPlayer()
-
-    def tearDown(self):
-        self.player.stop()
+class TestGameOfLife(SeleniumTestPlayer):
 
     def test_board(self):
         """ The page should show a board with 30x30 fields. The population of a square can be toggled via mouse click. """
-        squares = self.player.get_squares()
+        squares = self.get_squares()
         self.assertEqual(len(squares), 30*30)
 
-        self.assertFalse(self.player.is_square_populated(55))
-        self.assertFalse(self.player.is_square_populated(121))
+        self.assert_not_populated(squares[55])
+        self.assert_not_populated(squares[121])
 
-        self.player.click_square(55)
-        self.assertTrue(self.player.is_square_populated(55))
-        self.assertFalse(self.player.is_square_populated(121))
-
-        self.player.click_square(55)
-        self.assertFalse(self.player.is_square_populated(55))
-        self.assertFalse(self.player.is_square_populated(121))
+        squares[55].click()
+        self.assert_populated(squares[55])
+        self.assert_not_populated(squares[121])
+        
+        squares[55].click()
+        self.assert_not_populated(squares[55])
+        self.assert_not_populated(squares[121])
 
 
 if __name__ == '__main__':
